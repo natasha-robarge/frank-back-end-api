3d32fff553cc208bbe4701b6c599102bfebb7a7f
Natasha Robarge <robarge.natasha7@gmail.com>
2017-12-13 10:35:12 -0600

    Fixed typo in server

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
 require('dotenv').config();
 
-mongoose.connect(process.env.DB_CONN);
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-const userRoute = require('./routes/users');
 
 // Handling routes
 
 app.get('/', (req, res) => {
   res.send('Howdy');
 })
+
+const userRoute = require('./routes/users');
+
+app.post('/signup', userRoute.saveUser);
 
 // Listen to port
 
